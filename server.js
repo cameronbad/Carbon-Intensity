290418a4b1a18a86a7fddea75df67b5e73edb810
Cameron <145351508+cameronbad@users.noreply.github.com>
2026-09-03 17:12:19 +0100

    created initial repository

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("."));
 
-app.get("/api/carbon-intensity", (req, res) => {
+/* app.get("/api/carbon-intensity", (req, res) => {
     res.json({
         carbon_intensity: 245,
         unit: "gCO2/kWh"
     });
-});
+}); */
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
