6445f0537b40aeca6a4173102475a28e0bce7be5
Cameron <145351508+cameronbad@users.noreply.github.com>
2026-09-06 16:42:34 +0100

    Attempting to fix deployment

--- server.js
+++ server.js
@@ -89,6 +89,6 @@ app.get("/api/carbon-intensity", async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
